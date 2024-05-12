6618c8a5393fa11b7240af874b690b81431fd741
OkanShr <okan.shr@hotmail.com>
2024-05-12 21:00:57 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,19 +12,31 @@ with open("animals_template.html", "r") as template_file:
 
 animals_data = load_data('animals_data.json')
 
-output = ""
-for animal in animals_data:
-    if all(field in animal for field in ["name", "characteristics"]):
-        output += f"Name: {animal['name']}\n"
-        if "characteristics" in animal and "diet" in animal["characteristics"]:
-            output += f"Diet: {animal['characteristics']['diet']}\n"
-        if "locations" in animal and animal["locations"]:
-            output += f"Location: {animal['locations'][0]}\n"
-        if "characteristics" in animal and "type" in animal["characteristics"]:
-            output += f"Type: {animal['characteristics']['type'].capitalize()}\n"
-        output += "\n"
 
-new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+def generate_repository(animals):
+    """ Generates the output for the animals and creates
+        a new animals.html file replacing the
+        __REPLACE_ANIMALS_INFO__ from animals_template.html
+    """
+    output = ""
+    for animal in animals:
+        if all(field in animal for field in ["name", "characteristics"]):
+            output += "<li class=\"cards__item\">\n"
+            output += f"  <div class=\"card__title\">{animal['name']}</div>\n"
+            output += "  <p class=\"card__text\">\n"
+            if "locations" in animal and animal["locations"]:
+                output += f"      <strong>Location:</strong> {', '.join(animal['locations'])}<br/>\n"
+            if "characteristics" in animal and "type" in animal["characteristics"]:
+                output += f"      <strong>Type:</strong> {animal['characteristics']['type'].capitalize()}<br/>\n"
+            if "characteristics" in animal and "diet" in animal["characteristics"]:
+                output += f"      <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+            output += "  </p>\n"
+            output += "</li>\n"
 
-with open("animals.html", "w") as html_file:
-    html_file.write(new_html_content)
+    new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w") as html_file:
+        html_file.write(new_html_content)
+
+
+generate_repository(animals_data)
